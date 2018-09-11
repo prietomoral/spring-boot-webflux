cc9e6ca702edefbace3b5c8a14a5741cbc968a72
Luis <prietomoral@gmail.com>
2018-09-11 22:07:32 +0200

    WebFlux antes de crear controlador y vista

--- src/main/java/com/bolsaideas/springboot/webflux/SpringBootWebfluxApplication.java
+++ src/main/java/com/bolsaideas/springboot/webflux/SpringBootWebfluxApplication.java
@@ -1,19 +1,47 @@
 package com.bolsaideas.springboot.webflux;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+
+import com.bolsaideas.springboot.webflux.models.dao.ProductoDao;
+import com.bolsaideas.springboot.webflux.models.documents.Producto;
+
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootWebfluxApplication implements CommandLineRunner{
+public class SpringBootWebfluxApplication implements CommandLineRunner {
 
-	public static void main(String[] args)  {
-		SpringApplication.run(SpringBootWebfluxApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
+
+    @Autowired
+    private ProductoDao dao;
+
+    @Autowired
+    private ReactiveMongoTemplate mongoTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxApplication.class, args);
+    }
 
     @Override
     public void run(String... args) throws Exception {
-        // TODO Auto-generated method stub
-        
+        mongoTemplate.dropCollection("productos").subscribe();
+
+        Flux.just(new Producto("TV Panasonic Pantalla LCD", 456.89), new Producto("Sony Camara HD Digital", 177.89),
+                new Producto("Apple iPod", 46.89), new Producto("Sony Notebook", 846.89),
+                new Producto("Hewlett Packard Multifuncional", 200.89), new Producto("Bianchi Bicicleta", 70.89),
+                new Producto("HP Notebook Omen 17", 2500.89), new Producto("Mica Cómoda 5 Cajones", 150.89),
+                new Producto("TV Sony Bravia OLED 4K Ultra HD", 2255.89)).flatMap(producto -> {
+                    producto.setCreateAt(new Date());
+                    return dao.save(producto);
+                }).subscribe(producto -> log.info("Insert: " + producto.getId() + " " + producto.getNombre()));
+
     }
 }
